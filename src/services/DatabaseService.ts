763b88285571c276a9ebe3113379665ad55ffe3f
Farshad Bolouri <bolouri.farshad@gmail.com>
2024-11-02 20:15:12 +0330

    Update HabitDatabase using Dexie to manage habits and habit entries in IndexedDB

--- src/services/DatabaseService.ts
+++ src/services/DatabaseService.ts
@@ -1,27 +1,16 @@
-import Dexie from 'dexie';
+import Dexie, { Table } from 'dexie';
 import { Habit, HabitEntry } from '../models/Habit';
 
-class HabitDatabase extends Dexie {
-  habits: Dexie.Table<Habit, number>;
-  entries: Dexie.Table<HabitEntry, number>;
+export class HabitDatabase extends Dexie {
+  habits!: Table<Habit>;
+  habitEntries!: Table<HabitEntry>;
 
   constructor() {
     super('HabitTrackerDB');
     this.version(1).stores({
-      habits: '++id, name, frequency',
-      entries: '++id, habitId, date'
+      habits: '++id, name, frequency, targetCount, createdAt',
+      habitEntries: '++id, habitId, date, completed'
     });
-
-    this.habits = this.table('habits');
-    this.entries = this.table('entries');
-  }
-
-  async addHabit(habit: Habit): Promise<number> {
-    return this.habits.add(habit);
-  }
-
-  async logHabitEntry(entry: HabitEntry): Promise<number> {
-    return this.entries.add(entry);
   }
 }
 
